e9a5f61efd584f6824e2ba2e37da8387bb5d071a
Anton <tzat.anton@gmail.com>
2015-09-06 19:39:05 +0300

    New responcive design!

--- game.js
+++ game.js
@@ -1,134 +1,158 @@
-var requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame ||
-        window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;
-var canvas = document.getElementById("canvas-id");
-canvas.width = innerWidth;
-canvas.height = innerHeight;
-var context = canvas.getContext("2d");
+var game=(function(){
 
-var particles = [];
-
-var input = {
-	size:1,
-	number:500,
-	stickiness:3000,
-	speed:0.3,
-	spawntime:0,
-	alpha : 0.03
-}
-
-var hueshift = 360/input.number;
-var spawn={x:3.1415,y:-1};
-var h = 0;
-var hold;
-var mouse = {x:canvas.width/2,y:canvas.height/2}
-
-canvas.addEventListener("mousedown",function(e){
-if (spawn.x==3.1415){setTimeout(summon,5);}
-spawn.x =e.layerX;
-spawn.y = e.layerY;
-hold = true;
-})
-canvas.addEventListener("mouseup",function(e){
-hold = false;
-})
-
-function reset(){
-context.clearRect(0,0,canvas.width,canvas.height);
-particles=[];
-spawn.x=3.1415;
-}
-
-function particle(){
-	this.x = spawn.x;
-	this.y = spawn.y;
-	this.ax = 0;
-	this.ay = 0;
-	this.hue=Math.round(h);
-	h+=360/input.number;
-	//this.speed = (this.hue/300)*input.stickiness + input.speed;
-	//this.speed = this.hue/input.stickiness + input.speed; <--working
-}
-
-
-function control(name,val){
-switch(name){
-	case "size" :
-		input.size = val;
-		break;
-	case "number":
-		input.number = val;
-		break;
-	case "stickiness":
-		input.stickiness = val;
-		break;
-	case "speed":
-		input.speed = val;
-		break;
-	case "spawntime":
-		input.spawntime = val;
-		break;
-	case "alpha":
-		input.alpha = val;
-		break;
-
-}
-
-}
-
-function summon(){
-	if (particles.length>= input.number)return;
-	var count;
-	if (input.spawntime==0)
-		count=input.number;
-	else
-		count = input.number/(input.spawntime*1000/5)
-	//var count = (input.spawntime*5000)/input.number;
-	//count = input.number;
-	if (count==0){count=input.number;}
-		for (var a=0;a<count;a++){
-		particles.push(new particle());
-		}
-	setTimeout(summon, 5);
-}
-
-canvas.addEventListener("mousemove", function (args) {	
-	mouse.x = args.layerX;
-	mouse.y = args.layerY;
-	if (hold){
-	spawn.x = args.layerX;
-	spawn.y = args.layerY;
-	}
-
-}, false);
-
-
-function update() {
-	for (var a=0;a<particles.length;a++){
-		var angle = Math.atan2(particles[a].x-mouse.x,particles[a].y-mouse.y);
-		speed = particles[a].hue/input.stickiness + input.speed;
-		particles[a].ax+=-Math.sin(angle)*speed;
-		particles[a].ay+=-Math.cos(angle)*speed;
-		particles[a].x+=particles[a].ax;
-		particles[a].y+=particles[a].ay;
-	}
-	setTimeout(update, 10);
-}
-
-
-function draw() {
-	context.globalCompositeOperation = 'source-over';
-	context.fillStyle="rgba(0,0,0,0.01)"
-	context.fillRect(0,0,canvas.width,canvas.height);
-	context.globalCompositeOperation = 'lighter';
-	for (var a=0;a<particles.length;a++){
+	var canvas,context,animate;
 	
-		context.fillStyle = "hsla("+particles[a].hue+",100%,50%,"+input.alpha+")";
-		//context.beginPath();
-		//context.arc(particles[a].x,particles[a].y,input.size,0,Math.PI*2);
-		context.fillRect(particles[a].x,particles[a].y,input.size,input.size);
-		//context.fill();
+	var input = {
+		size:1,
+		number:500,
+		stickiness:3000,
+		speed:0.3,
+		spawntime:0,
+		alpha : 0.03,
+		light : 0
 	}
-    requestAnimationFrame(draw);
-}
-update();
-draw();
+	
+	var particles = [];
+	var hueshift = 360/input.number;
+	var spawn={x:3.1415,y:-1};
+	var h = 0;
+	var hold;
+	var mouse;
+	
+	
+	
+	var init = function(){
+		canvas = canvasControl.getCanvas();
+		context = canvasControl.getContext();
+		animate = canvasControl.requestAnimationFrame();
+		
+		pubsub.emit("controlReady");
+		pubsub.emit("resetReady");
+		mouse = {x:canvas.width()/2,y:canvas.height()/2}
+		
+		bindEvents();
+		update();
+		draw();
+		
+	}
+	
+	var bindEvents = function(){
+	
+		canvas.on("mousedown", function (e){
+			if (spawn.x==3.1415){setTimeout(summon,5);}
+				var parentOffset = $(this).offset(); 
+				spawn.x = e.pageX - parentOffset.left;
+				spawn.y =  e.pageY - parentOffset.top;
+				hold = true;
+		});
+		
+		canvas.on("mouseup", function() {
+			hold = false;
+		});
+		
+		canvas.on("mousemove", function(e) {
+		var parentOffset = $(this).offset(); 
+			mouse.x = e.pageX - parentOffset.left;
+			mouse.y = e.pageY - parentOffset.top;
+			if (hold){
+				spawn.x =  mouse.x;
+				spawn.y =  mouse.y;
+			}
+		});
+
+	}
+	
+	function particle(){
+		this.x = spawn.x;
+		this.y = spawn.y;
+		this.ax = 0;
+		this.ay = 0;
+		this.hue=Math.round(h);
+		h+=360/input.number;
+		//this.speed = this.hue/input.stickiness + input.speed; <--working
+	}
+	
+	
+	function reset(){
+		context.clearRect(0,0,canvas.width,canvas.height);
+		particles=[];
+		spawn.x=3.1415;
+	}
+	
+	
+	
+	
+	function summon(){
+		if (particles.length>= input.number)
+			return;
+		var count;
+		if (input.spawntime==0)
+			count=input.number;
+		else
+			count = input.number/(input.spawntime*1000/5)
+		if (count==0)
+			count=input.number;
+		for (var a=0;a<count;a++)
+			particles.push(new particle());
+		setTimeout(summon, 5);
+	}
+	
+	function control(name,val){
+		switch(name){
+			case "size" :
+				input.size = val;
+				break;
+			case "number":
+				input.number = val;
+				break;
+			case "stickiness":
+				input.stickiness = val;
+				break;
+			case "speed":
+				input.speed = val;
+				break;
+			case "spawntime":
+				input.spawntime = val;
+				break;
+			case "alpha":
+				input.alpha = val;
+				break;
+			case "light":
+				input.light = val;
+				break;
+		}
+	}
+	
+	
+
+	function update() {
+		for (var a=0;a<particles.length;a++){
+			var angle = Math.atan2(particles[a].x-mouse.x,particles[a].y-mouse.y);
+			speed = particles[a].hue/input.stickiness + input.speed;
+			particles[a].ax+=-Math.sin(angle)*speed;
+			particles[a].ay+=-Math.cos(angle)*speed;
+			particles[a].x+=particles[a].ax;
+			particles[a].y+=particles[a].ay;
+		}
+		setTimeout(update, 10);
+	}
+
+	function draw() {
+		if (input.light!=0){
+			context.globalCompositeOperation = 'source-over';
+			context.fillStyle="rgba(0,0,0,0.01)"
+			context.fillRect(0,0,canvas.width,canvas.height);
+		}
+		context.globalCompositeOperation = 'lighter';
+		for (var a=0;a<particles.length;a++){
+			context.fillStyle = "hsla("+particles[a].hue+",100%,50%,"+input.alpha+")";
+			context.fillRect(particles[a].x,particles[a].y,input.size,input.size);
+		}
+		animate(draw);
+	}
+	
+	pubsub.on("canvasReady",init);
+	
+	
+	
+})()
---
+++ init.js
@@ -0,0 +1,135 @@
+$(document).ready( function(){
+	window.canvasControl=(function(){
+	
+		var requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame ||
+        window.webkitRequestAnimationFrame || window.msRequestAnimationFrame ;
+		var c=$('#canvas-id');
+		var context= c.get(0).getContext('2d');
+		var container= $(c).parent();
+		var respondCanvas=function(e){ 
+			c.attr('width', $(container).width() );
+			c.attr('height', $(container).height() ); 
+
+		}
+		var init=function(){
+			bindEvents();
+			respondCanvas();
+		}
+		
+		var bindEvents=function(){
+			$(window).on("resize", respondCanvas);
+			pubsub.on("canvasDestroy",destroy);
+		}
+		var destroy=function(){
+			$(window).off("resize", respondCanvas);
+			pubsub.off("canvasDestroy",destroy)
+		}
+		var getContext=function(){
+			return context;
+		}
+		var getAnimationFrame=function(){
+			return requestAnimationFrame;
+		}
+		var getCanvas=function(){
+			return c;
+		}
+		init();
+		
+		return{
+			getContext:getContext,
+			requestAnimationFrame:getAnimationFrame,
+			getCanvas:getCanvas,
+			destroy:destroy
+		}
+	})()
+	
+	pubsub.on("controlReady",function(){
+		console.log("hi!");
+		function handler(event, ui){
+			control($(this).attr("id"),$(this).slider("option", "value"))
+		}
+	
+		$("#size").slider({
+		min:1,
+		max:20,
+		step:1,
+		value:1,
+		change: handler
+		});
+		$("#number").slider({
+		min:1,
+		max:2000,
+		step:10,
+		value:500,
+		change: handler
+		});
+		$("#stickiness").slider({
+		min:500,
+		max:10000,
+		step:100,
+		value:3000,
+		change: handler
+		});
+		$("#speed").slider({
+		min:0.1,
+		max:2,
+		step:0.01,
+		value:0.3,
+		change: handler
+		});
+		$("#spawntime").slider({
+		min:0,
+		max:5,
+		value:0,
+		step:0.5,
+		change: handler
+		});
+		$("#alpha").slider({
+		min:0,
+		max:1,
+		value:0.03,
+		step:0.01,
+		change: handler
+		});
+		$("#light").slider({
+		min:0,
+		max:1,
+		value:0,
+		step:0.01,
+		change: handler
+		});
+		
+		pubsub.on("resetSet",function(){
+			$("#cls").button().click(reset);;
+		});
+		
+	});
+	
+	
+		$(".controls").hide();
+		var r= $(".controls").is(":visible")?90:270
+		var rotate = 'rotate(' + r + 'deg)';
+			$(".open > img").css({ 
+				'-webkit-transform': rotate,
+				'-moz-transform': rotate,
+				'-o-transform': rotate,
+				'-ms-transform': rotate,
+				'transform': rotate 
+		});
+
+	$(".open").click(function(){
+		$(".controls").toggle();
+		var r= $(".controls").is(":visible")?90:270
+		var rotate = 'rotate(' + r + 'deg)';
+		$(".open > img").css({ 
+			'-webkit-transform': rotate,
+			'-moz-transform': rotate,
+			'-o-transform': rotate,
+			'-ms-transform': rotate,
+			'transform': rotate 
+		});
+	});
+	
+	pubsub.emit("canvasReady");
+	
+}); 
